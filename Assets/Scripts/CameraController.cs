395f06085bb19fcd244a5aed1f13751bceff0492
Raymond <20248577+javaarchive@users.noreply.github.com>
2023-03-02 11:38:35 -0800

    partial camera toggle

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public GameObject car;
+    public float lastCameraToggle = 0f;
+
+    public Vector3 camOffset = new Vector3(0, 3, -8);
+    public Vector3 camOffsetFirstPerson = new Vector3(0, -2, 5);
+    public bool isFirstPerson = false;
 
     // Start is called before the first frame update
     void Start()
@@ -15,7 +20,15 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = car.transform.position + car.transform.rotation * (new Vector3(0, 3, -8));
+        if(Input.GetKey("shift") && (Time.deltaTime - lastCameraToggle) > 1){
+            lastCameraToggle = Time.deltaTime;
+            isFirstPerson = !isFirstPerson;
+        }
+        if(isFirstPerson){
+            transform.position = car.transform.position + car.transform.rotation * camOffset;
+        }else{
+            transform.position = car.transform.position + car.transform.rotation * camOffsetFirstPerson;
+        }
         transform.rotation = car.transform.rotation;
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,12 +3,18 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class PlayerController : MonoBehaviour {
-	void Start(){
+	Rigidbody rigidBody;
 
+	void Start(){
+		rigidBody = GetComponent<Rigidbody>();
+		rigidBody.centerOfMass = new Vector3(0,-1,0);
 	}
 
 	void Update(){
-		transform.Translate(Input.GetAxis ("Vertical") * transform.forward * Time.deltaTime * 15, Space.World);	
+		float forwardForce = Input.GetAxis ("Vertical") * Time.deltaTime * 1000;
+		//  transform.forward
+		rigidBody.AddForce((forwardForce * transform.forward), ForceMode.Acceleration);
+		// transform.Translate(Input.GetAxis ("Vertical") * transform.forward * Time.deltaTime * 15, Space.World);	
 		transform.Rotate(0,Input.GetAxis("Horizontal"),0);
 	}
 }
